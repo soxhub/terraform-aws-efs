cf973dfd01d99129fd2e97bbbcb9ce1f8b013055
Brian Tai <btai@soxhub.com>
2020-02-13 12:45:42 -0800

    return mount target ids/ips

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,13 @@ output "dns" {
   description = "the dns name of the EFS file system"
   value       = "${aws_efs_file_system.efs.dns_name}"
 }
+
+output "mount_target_ids" {
+  description = "the list of mount target ids"
+  value       = ["${aws_efs_mount_target.efs_mount_targets.*.id}"]
+}
+
+output "mount_target_ips" {
+  description = "the list of mount target ips"
+  value       = ["${aws_efs_mount_target.efs_mount_targets.*.id}"]
+}
