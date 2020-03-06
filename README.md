cded9cab74d7ff4d42a5a8ff27c19a07a14aac7c
Kevin Stenerson <kstenerson@auditboard.com>
2020-03-06 11:55:02 -0800

    Upgrade to terraform 12

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# terraform-aws-efs
+# Terraform: AWS EFS (Elastic File System)
+
+This provides the `efs` terraform module for the AWS provider.
+
+This provisions an AWS EFS.
+
+## Usage
+In your `.tf` file, import the module:
+
+```terraform
+module "efs" {
+  source  = "app.terraform.io/auditboard/efs/aws"
+  version = "1.1.0"
+
+  name       = local.name
+  region     = local.region
+  subnet_ids = module.vpc.private_subnet_ids
+}
+```
--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,25 @@
 output "id" {
   description = "the id of the EFS File system"
-  value       = "${aws_efs_file_system.efs.id}"
+  value       = aws_efs_file_system.efs.id
 }
 
 output "arn" {
   description = "the arn of the EFS file system"
-  value       = "${aws_efs_file_system.efs.arn}"
+  value       = aws_efs_file_system.efs.arn
 }
 
 output "dns" {
   description = "the dns name of the EFS file system"
-  value       = "${aws_efs_file_system.efs.dns_name}"
+  value       = aws_efs_file_system.efs.dns_name
 }
 
 output "mount_target_ids" {
   description = "the list of mount target ids"
-  value       = ["${aws_efs_mount_target.efs_mount_targets.*.id}"]
+  value       = [aws_efs_mount_target.efs_mount_targets.*.id]
 }
 
 output "mount_target_ips" {
   description = "the list of mount target ips"
-  value       = ["${aws_efs_mount_target.efs_mount_targets.*.ip_address}"]
+  value       = [aws_efs_mount_target.efs_mount_targets.*.ip_address]
 }
+
